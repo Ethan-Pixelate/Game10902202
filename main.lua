b6b6b05eee0f838027c31413e39e0ade8bde8e89
BuildTools <unconfigured@null.spigotmc.org>
2022-09-01 20:08:07 -0600

    first commit, time to make some bullshit :sunglasses:

---
+++ main.lua
@@ -0,0 +1,16 @@
+function love.load()
+    love.graphics.setDefaultFilter("nearest","nearest")
+    
+end
+
+function love.update(DeltaTime)
+
+end
+
+function love.draw()
+
+end
+
+function love.resize(W, H)
+
+end
